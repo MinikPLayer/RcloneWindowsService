b28d9715116556dd0c90f1d2ce5013e62911f1eb
Michał Tomecki <minexplaypl@gmail.com>
2024-12-04 03:33:46 +0100

    Add project files.

---
+++ RcloneWindowsService/RcloneService.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Security;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RcloneWindowsService
+{
+    public partial class RcloneService : ServiceBase
+    {
+        Process rcloneProcess = null;
+        string serviceName = "";
+
+        public RcloneService()
+        {
+            InitializeComponent();
+
+            this.ServiceName = "Rclone Windows Service";
+        }
+
+        protected string GetServiceName()
+        {
+            int processId = Process.GetCurrentProcess().Id;
+            var query = "SELECT * FROM Win32_Service where ProcessId = " + processId;
+            System.Management.ManagementObjectSearcher searcher =
+                new System.Management.ManagementObjectSearcher(query);
+
+            foreach (System.Management.ManagementObject queryObj in searcher.Get())
+            {
+                return queryObj["Name"].ToString();
+            }
+
+            throw new Exception("Can not get the ServiceName");
+        }
+
+        private string GetDisplayName()
+        {
+            ServiceController sc = new ServiceController(GetServiceName());
+            var displayName = sc.DisplayName;
+            return displayName;
+        }
+
+        void Log(string log, EventLogEntryType type = EventLogEntryType.Information, int id = 101, short category = 1)
+        {
+            using (EventLog eventLog = new EventLog("Application"))
+            {
+                eventLog.Source = serviceName;
+                eventLog.WriteEntry(log, type, id, category);
+            }
+        }
+
+        private string CreateEventSource()
+        {
+            string eventSource = serviceName;
+            bool sourceExists;
+            try
+            {
+                // searching the source throws a security exception ONLY if not exists!
+                sourceExists = EventLog.SourceExists(eventSource);
+                if (!sourceExists)
+                {   // no exception until yet means the user as admin privilege
+                    EventLog.CreateEventSource(eventSource, "Application");
+                }
+            }
+            catch (SecurityException)
+            {
+                eventSource = "Application";
+            }
+
+            return eventSource;
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            serviceName = GetServiceName();
+            CreateEventSource();
+
+            var programArgs = Environment.GetCommandLineArgs().ToList();
+            // Remove the first argument which is the path to the executable
+            programArgs.RemoveAt(0);
+
+            var now = DateTime.Now;
+            var argsString = "";
+            foreach (var arg in programArgs)
+            {
+                argsString += $"\"{arg}\" ";
+            }
+            var command = "rclone";
+            var commandArgs = $"mount {argsString}";
+
+            rcloneProcess = new Process();
+            rcloneProcess.StartInfo.FileName = command;
+            rcloneProcess.StartInfo.Arguments = commandArgs;
+            rcloneProcess.StartInfo.UseShellExecute = false;
+            rcloneProcess.StartInfo.RedirectStandardOutput = true;
+            rcloneProcess.StartInfo.RedirectStandardError = true;
+            rcloneProcess.Exited += RcloneProcess_Exited;
+            rcloneProcess.OutputDataReceived += RcloneProcess_OutputDataReceived;
+            rcloneProcess.ErrorDataReceived += RcloneProcess_ErrorDataReceived;
+            rcloneProcess.EnableRaisingEvents = true;
+            rcloneProcess.Start();
+            rcloneProcess.BeginErrorReadLine();
+            rcloneProcess.BeginOutputReadLine();
+
+            Log($"Rclone service started. \nFull command: {command} {commandArgs}");
+        }
+
+        private void RcloneProcess_ErrorDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            Log("Rclone process output:\n" + e.Data, EventLogEntryType.Error, 104, 2);
+        }
+
+        private void RcloneProcess_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            Log("Rclone process output:\n" + e.Data, EventLogEntryType.Information, 103, 2);
+        }
+
+        private void RcloneProcess_Exited(object sender, EventArgs e)
+        {
+            Log("Rclone process exited unexpectedly.", EventLogEntryType.Error, 102);
+            rcloneProcess = null;
+            this.Stop();
+        }
+
+        protected override void OnStop()
+        {
+            if (rcloneProcess == null)
+                return;
+
+            if(rcloneProcess.HasExited)
+            {
+                Log("Rclone process has already exited.");
+                return;
+            }
+
+            rcloneProcess.Kill();
+            Log("Rclone service stopped.");
+        }
+    }
+}
---
+++ RcloneWindowsService/install.ps1
@@ -0,0 +1,84 @@
+<#
+.DESCRIPTION
+    Installs the service
+.PARAMETER Name
+    Name of the service to be installed.
+.PARAMETER Unattended
+	Disables the PAUSE after installation.
+.PARAMETER StartMode
+	Service start mode. Possible values: boot, system, auto, demand, disabled, delayed-auto
+.PARAMETER Params
+	Additional parameters to be passed to the rclone mount command.
+.PARAMETER Uninstall
+	Uninstall instead of installing.
+.EXAMPLE
+	C:\PS> .\install.ps1 -Name "Google Drive" -Params "gdrive:/ G: --rc"
+.EXAMPLE
+    C:\PS> .\install.ps1 -Name "Google Drive"
+	Installs the service as "Google Drive".
+.EXAMPLE
+    C:\PS> .\install.ps1 -Name "Something cloud" -Unattended
+	Installs the service as "Something cloud" without a PAUSE prompt.
+.EXAMPLE
+    C:\PS> .\install.ps1 -Name "Something cloud" -Uninstall
+	Uninstalls the service.
+.NOTES
+    Author: Michał Tomecki
+    Date:   04.12.2024    
+#>
+param(
+	[Parameter(mandatory=$true)]
+	[string] $Name,
+
+	[Parameter(mandatory=$true)]
+	[string] $Params,
+
+	[ValidateSet("boot", "system", "auto", "demand", "disabled", "delayed-auto")]
+	[string] $StartMode = "auto",
+
+	[switch] $Unattended = $false,
+	[switch] $Uninstall = $false
+)
+
+$location = "$PSScriptRoot\RcloneWindowsService.exe"
+if (!(Test-Path $location)) {
+	Write-Host "The file $location does not exist. Perhaps project is not compiled?"
+	Write-Host "Exiting"
+	exit
+}
+
+$serviceName = "rclone-" + $Name.ToLower().Replace(' ', '-')
+$displayName = "[rclone] $Name"
+$formattedParams = ($Params -replace "'", '"') -replace '"', '\"'
+
+if ($Uninstall) {
+	Write-Host "Uninstalling service..."
+	Write-Host "- Service Name: $serviceName"
+
+	if (!$Unattended) {
+		Read-Host -Prompt "Press any key to continue or CTRL+C to quit" | Out-Null
+	}
+
+	sc.exe delete $serviceName
+
+	if (!$unattended) {
+		PAUSE
+	}
+	exit
+}
+
+echo "Installing service..."
+echo "- Service Name: $serviceName"
+echo "- Display Name: $displayName"
+echo "- Full service command: rclone mount $formattedParams"
+echo "- Location: $location"
+
+if (!$Unattended) {
+	Read-Host -Prompt "Press any key to continue or CTRL+C to quit" | Out-Null
+}
+
+sc.exe create $serviceName binpath= "$location $formattedParams" start= $StartMode DisplayName= $displayName
+
+if (!$unattended) {
+	PAUSE
+}
